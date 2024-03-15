1ceb8670f8f88f8fc952bc14e10b369c305290ec
rihabzz <124267185+rihabzz@users.noreply.github.com>
2024-03-15 22:59:46 +0100

    debutanim

---
+++ script/animations.js
@@ -0,0 +1,15 @@
+gsap.from('.descrExpo', {
+    duration: 6, 
+    opacity: 0,
+
+        toggleActions: 'restart none none none' 
+    
+});
+
+gsap.from('.title', {
+    duration: 8, 
+    opacity: 0,
+        toggleActions: 'restart none none none',
+        delay: 0.4
+    
+});
