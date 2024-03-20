0cac9b89446dc98c79aac4d3728b44f140691385
rihabzz <124267185+rihabzz@users.noreply.github.com>
2024-03-20 16:56:12 +0100

    correctionBugJs

--- script/deroule.js
+++ script/deroule.js
@@ -7,7 +7,6 @@ dropdownBtn.addEventListener("click", function () {
 document.addEventListener("DOMContentLoaded", function () {
     var currentLang = "fr"; // par défaut=fr
 
-
     // recup le fichier JSON 
     fetch('./script/translations.json')
         .then(response => response.json())
@@ -15,60 +14,64 @@ document.addEventListener("DOMContentLoaded", function () {
             // fct pour chnger la langue
             function changeLanguage(lang) {
                 currentLang = lang;
-                updatePage(); // mise à jour du cntenu de la page après avoir changé la lg
+                updatePage(); // Mise à jour du contenu de la page après avoir changé la langue
                 localStorage.setItem("lang", lang);
+            
+                // Mettre à jour les valeurs des heures
+                updateHourValues(currentLang);
+
                 var submitBtn = document.querySelector('.submit');
-                if (lang === 'en') {
-                    submitBtn.value = "Confirm";
-                } else if (lang === 'fr') {
-                    submitBtn.value = "Valider";
-                }
-                updateHourValues(lang);
-
-
-            }
-
-            //  mettre a jour le contenu de la page en fct de la langue actuelle
-            function updatePage() {
-                var translations = data[currentLang];
-                for (var key in translations) {
-                    var translation = translations[key];
-                    var element;
-                    if (translation.class) {
-                        element = document.querySelector("." + translation.class);
-                    } else if (translation.id) {
-                        element = document.getElementById(translation.id);
+                if (submitBtn) {
+                    if (lang === 'en') {
+                        submitBtn.value = "Confirm";
+                    } else if (lang === 'fr') {
+                        submitBtn.value = "Valider";
                     }
-                    if (element) {
-                        // Remplace les sauts de ligne par des balises <br>
-                        translation.text = translation.text.replace(/\\n/g, '<br>');
-                        element.innerHTML = translation.text; // Utilise innerHTML pour interpréter les balises HTML
+                } 
+            }
+                //  mettre a jour le contenu de la page en fct de la langue actuelle
+                function updatePage() {
+                    var translations = data[currentLang];
+                    for (var key in translations) {
+                        var translation = translations[key];
+                        var element;
+                        if (translation.class) {
+                            element = document.querySelector("." + translation.class);
+                        } else if (translation.id) {
+                            element = document.getElementById(translation.id);
+                        }
+                        if (element) {
+                            // Remplacez les sauts de ligne par des balises <br>
+                            translation.text = translation.text.replace(/\\n/g, '<br>');
+                            element.innerHTML = translation.text; // Utilise innerHTML pour interpréter les balises HTML
+                        }
+
                     }
-
                 }
-            }
 
-            var savedLang = localStorage.getItem("lang");
-            if (savedLang) {
-                // utlise la langue enregistree pr afficher le contenu de la page
-                changeLanguage(savedLang);
-            } else {
-                // utlise 1 langue par défaut si aucune langue n'est enregistrée
-                updatePage(); //  langue par défaut définie dans currentLang
-            }
-
-            //  event pr le clic sur le bouton de langue
-            document.getElementById("dropdownContent").addEventListener("click", function (event) {
-                if (event.target.classList.contains("en")) {
-                    changeLanguage("en"); // chgmnt la langue en anglais
-                } else if (event.target.classList.contains("fr")) {
-                    changeLanguage("fr"); // chgmnt de la langue en français
+                var savedLang = localStorage.getItem("lang");
+                if (savedLang) {
+                    // utlise la langue enregistree pr afficher le contenu de la page
+                    changeLanguage(savedLang);
+                } else {
+                    // utlise 1 langue par défaut si aucune langue n'est enregistrée
+                    updatePage(); //  langue par défaut définie dans currentLang
                 }
-            });
-        })
+
+                //  event pr le clic sur le bouton de langue
+                document.getElementById("dropdownContent").addEventListener("click", function (event) {
+                    if (event.target.classList.contains("en")) {
+                        changeLanguage("en"); // chgmnt la langue en anglais
+                    } else if (event.target.classList.contains("fr")) {
+                        changeLanguage("fr"); // chgmnt de la langue en français
+                    }
+                });
+            })
         .catch(error => console.error('Erreur lors du chargement du fichier JSON :', error));
 
         function updateHourValues(lang) {
+            console.log("updateHourValues called with language: " + lang);
+        
             var heures = document.querySelectorAll('input[name="horaire"]');
             heures.forEach(function(heure) {
                 switch (heure.value) {
@@ -79,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         heure.value = lang === 'en' ? '11:00 AM' : '11:00';
                         break;
                     case '12:00':
-                        heure.value = lang === 'en' ? '12:00 AM' : '12:00';
+                        heure.value = lang === 'en' ? '12:00 PM' : '12:00';
                         break;
                     case '13:00':
                         heure.value = lang === 'en' ? '1:00 PM' : '13:00';
@@ -104,6 +107,5 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             });
         }
-
-    
+        
 });
